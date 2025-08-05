a4d7aa31743067bfe2d89b4f69722df84c19ad7c
Wojtek Galik <w.galik@poczta.fm>
2025-08-05 15:11:39 +0200

    update

--- script/script.js
+++ script/script.js
@@ -1,4 +1,32 @@
-$(document).ready(function() {
-  console.log(112);
-  
+$(document).ready(function () {
+  $("#hamburger").on("click", function () {
+    $(".nav-menu").toggleClass("active");
+    if ($(".nav-menu").hasClass("active")) {
+      $("#home").css("marginTop", 210);
+    } else {
+      $("#home").css("marginTop", 60);
+    }
+  });
+
+  $("#logo").on("click", function () {
+    if ($(".nav-menu").hasClass("active")) {
+      $(".nav-menu").removeClass("active");
+      $("#home").css("marginTop", 60);
+    }
+  });
+  $(".nav-menu li").on("click", function () {
+    if ($(".nav-menu").hasClass("active")) {
+      $(".nav-menu").removeClass("active");
+      $("#home").css("marginTop", 60);
+    }
+  });
+
+  $("#portfolio button").on("click", function (event) {
+    $("#portfolio button").css("textDecoration", "none");
+    const gridContainer = $(".grid-container");
+    gridContainer.hide();
+    gridContainer.removeClass().addClass(`grid-container ${event.target.id}`);
+    gridContainer.fadeIn(1000);
+    $(`#${event.target.id}`).css("textDecoration", "underline");
+  });
 });
